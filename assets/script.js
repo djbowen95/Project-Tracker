0f641a4615c7e15bff5c97765c01adb2636a3eb1
Daniel Bowen <djbowen95@gmail.com>
2023-12-04 19:56:19 +0000

    Import jQuery to index.html

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+const rootEl = $('#root');
+
+const heroEl = $('<header>');
+
+heroEl.attr('class', 'hero');
+heroEl.text("Welcome to the Project Tracker");
+// rootEl.append(heroEl);
+
+const cardEl = $('<section>');
+cardEl.text('Hi')
+rootEl.append(heroEl, cardEl);

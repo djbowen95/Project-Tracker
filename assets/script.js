79a8e882741a458e60a091abb625b5d1b5dc7fba
Daniel Bowen <djbowen95@gmail.com>
2023-12-04 20:28:53 +0000

    Build all HTML elements for the instruction card section

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,16 @@
-// Root div to contain all sections.
+// Select the root div from the HTML.
 const rootEl = $("#root");
 
-// Four core sections of the page.
+// Render the four core sections of the page.
 const heroEl = $("<header>");
 const cardEl = $("<section>");
 const modalEl = $("<section>");
 const tableEl = $("<section>");
 rootEl.append(heroEl, cardEl, modalEl, tableEl);
 
-// Hero element scripts. 
+// Hero section: render welcome text and display current date.
 const welcomeEl = $("<h1>");
-const dateEl = $("<h2>");
+const dateEl = $("<time>"); // Might change to time html tag
 
 heroEl.attr("class", "hero");
 heroEl.append(welcomeEl, dateEl);
@@ -18,9 +18,20 @@ heroEl.append(welcomeEl, dateEl);
 welcomeEl.text("Welcome to the Project Tracker");
 dateEl.text("Timer placeholder"); // This will be replaced with a function that uses moment.js to display time.
 
+// Instruction card section: header, instructions, and button to launch modal.
+const cardHeaderEl = $("<h2>");
+const instructionsEl = $("<p>");
+const newProjectButton = $("<button>");
 
-cardEl.text("This section will contain instructions");
+cardEl.attr("class", "instructionCard");
+cardEl.append(cardHeaderEl, instructionsEl, newProjectButton);
 
+cardHeaderEl.text("How to Use");
+instructionsEl.text("Click the button below, and complete the form, to add a new project.");
+newProjectButton.text("New Project");
+
+// Modal section: form for a new project.
 modalEl.text("This section will contain a form");
 
+// Table section: table of existing projects.
 tableEl.text("This section will show a table");

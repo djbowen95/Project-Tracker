e9e008eb830334153035544f4d01731a63aabac2
Daniel Bowen <djbowen95@gmail.com>
2023-12-04 19:56:41 +0000

    Use jQuery to create core sections of page

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,16 @@
-const rootEl = $('#root');
+const rootEl = $("#root");
 
-const heroEl = $('<header>');
+const heroEl = $("<header>");
+const cardEl = $("<section>");
+const modalEl = $("<section>");
+const tableEl = $("<section>");
+rootEl.append(heroEl, cardEl, modalEl, tableEl);
 
-heroEl.attr('class', 'hero');
+heroEl.attr("class", "hero");
 heroEl.text("Welcome to the Project Tracker");
-// rootEl.append(heroEl);
 
-const cardEl = $('<section>');
-cardEl.text('Hi')
-rootEl.append(heroEl, cardEl);
+cardEl.text("This section will contain instructions");
+
+modalEl.text("This section will contain a form");
+
+tableEl.text("This section will show a table");

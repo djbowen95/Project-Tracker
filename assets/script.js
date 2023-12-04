4f6b429cf8458264c882119768dc17f8b650564d
Daniel Bowen <djbowen95@gmail.com>
2023-12-04 20:03:27 +0000

    Build all elements required for the header

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,23 @@
+// Root div to contain all sections.
 const rootEl = $("#root");
 
+// Four core sections of the page.
 const heroEl = $("<header>");
 const cardEl = $("<section>");
 const modalEl = $("<section>");
 const tableEl = $("<section>");
 rootEl.append(heroEl, cardEl, modalEl, tableEl);
 
+// Hero element scripts. 
+const welcomeEl = $("<h1>");
+const dateEl = $("<h2>");
+
 heroEl.attr("class", "hero");
-heroEl.text("Welcome to the Project Tracker");
+heroEl.append(welcomeEl, dateEl);
+
+welcomeEl.text("Welcome to the Project Tracker");
+dateEl.text("Timer placeholder"); // This will be replaced with a function that uses moment.js to display time.
+
 
 cardEl.text("This section will contain instructions");
 
